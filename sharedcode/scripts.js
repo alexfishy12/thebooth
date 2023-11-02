938dcd2ade514851062541103cf021903dd2c670
Healiam <83127643+Healiam@users.noreply.github.com>
2023-11-02 11:46:01 -0400

    Scrap Login Popup
    Login page returned to its own page. Having it as a popup in a separate window causes many problems

--- sharedcode/scripts.js
+++ sharedcode/scripts.js
@@ -1,13 +1,12 @@
-//Load Shared Code into file on startup
+//Load shared navbar into container
 $(document).ready(function () {
     $('#navbar-container').load('../sharedcode/nav.html', function() {
         const loginButton = document.getElementById("loginButton");
         if (loginButton) {
             loginButton.addEventListener("click", function() {
-                const loginWindow = window.open("../sharedcode/login.html", "Login", "width=400, height=300");
-                loginWindow.focus();
+                const loginPageUrl = "../webpages/login.html";
+                window.location.href = loginPageUrl;
             });
         }
     });
 });
-
